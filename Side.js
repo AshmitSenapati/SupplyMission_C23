f6927085595f2ea95a4a5373905001c894c06a31
AshmitSenapati <71175726+AshmitSenapati@users.noreply.github.com>
2020-11-16 08:12:53 +0530

    Add files via upload

---
+++ Side.js
@@ -0,0 +1,26 @@
+class side {
+
+constructor(x,y,w,h) {
+
+var options = {
+isStatic: true
+
+}
+this.body = Bodies.rectangle(x,y,w,h,options)
+//this.body.shapeColor = "red"
+this.width = w;
+this.height = h;
+
+//World.add(world,this.body)
+}
+display() {
+var pos = this.body.position
+rectMode(CENTER);
+rect(pos.x,pos.y,this.width,this.height);
+
+
+}
+
+
+
+}
